8b419eb554c9a192edc4b0e16c218a2596aa5b66
elaabous <elaabousfati.2019@gmail.com>
2025-07-19 14:34:07 -0100

    dashnord admin

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,22 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\UnitController;
 use App\Http\Middleware\CompanyAccess;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DashboardController;
 
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', CompanyAccess::class])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', CompanyAccess::class])
+    ->name('dashboard');
+
 
 Route::middleware(['auth', CompanyAccess::class])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/user/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/user/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/user/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
     Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
